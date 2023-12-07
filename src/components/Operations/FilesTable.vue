504e1919c25fa9f6db3349f91f797eb9b0ab2fa8
JasonGrass <junjiegrass@qq.com>
2023-12-07 09:57:26 +0800

    使用 vxe-table 代替 element table

--- src/components/Operations/FilesTable.vue
+++ src/components/Operations/FilesTable.vue
@@ -1,64 +1,69 @@
 <template>
     <div>
         <div class="filters">
-            <el-checkbox v-model="colHideFolder" label="隐藏目录" border />
+            <el-checkbox v-model="isHideFolder" label="隐藏目录" border />
+            <el-checkbox v-model="isOnlyPreview" label="仅显示预览" border />
         </div>
-        <!-- el-table 有一个布局 BUG，如果 column 没有设置 width，则会自适应宽度，但这个宽度只会变大，窗口大小缩小之后，此宽度不会自适应缩小 -->
-        <el-table :data="tableData" ref="fileTable" class="table" max-height="2000px" stripe flexible table-layout="auto">
-            <el-table-column type="index" :index="(index) => index + 1" label="序号" width="60" align="center" />
-            <el-table-column prop="file" min-width="50%" label="文件名" align="right" />
-            <el-table-column prop="modifyTime" :formatter="timeFormater" label="修改时间" width="180" align="center" />
-            <el-table-column prop="createTime" :formatter="timeFormater" label="创建时间" width="180" align="center" />
-            <el-table-column prop="size" :formatter="sizeFormatter" label="大小" width="100" align="center" />
-            <el-table-column v-if="!colHideFolder" prop="folder" min-width="50%" label="目录" align="right" />
-            <el-table-column prop="preview" min-width="50%" label="预览" align="right" />
-        </el-table>
+
+        <vxe-table :data="tableData" class="table" max-height="300%" stripe border="inner" empty-text="尚未加载任何文件">
+            <vxe-column type="seq" title="序号" width="60" align="center"></vxe-column>
+            <vxe-column field="file" title="文件名" sortable align="right"></vxe-column>
+            <vxe-column :visible="!isOnlyPreview" field="modifyTime" :formatter="timeFormater" title="修改时间" width="180"
+                sortable align="center"></vxe-column>
+            <vxe-column :visible="!isOnlyPreview" field="createTime" :formatter="timeFormater" title="创建时间" width="180"
+                sortable align="center"></vxe-column>
+            <vxe-column :visible="!isOnlyPreview" field="size" :formatter="sizeFormatter" title="大小" width="100" sortable
+                align="center"></vxe-column>
+            <vxe-column :visible="!isHideFolder && !isOnlyPreview" field="folder" title="目录" sortable
+                align="right"></vxe-column>
+            <vxe-column field="preview" title="预览" align="right"></vxe-column>
+
+        </vxe-table>
+
     </div>
 </template>
 
 <script lang="ts" setup>
-import type { TableColumnCtx } from 'element-plus'
 import { formatFileSize, formatDate } from "@/utils/formatter";
+import { VxeColumnPropTypes } from 'vxe-table'
 
-const sizeFormatter = (row: FileItem, _column: TableColumnCtx<FileItem>, _cellVaule: number | undefined, _index: number) => {
-    return formatFileSize(row.size)
+import { tableData } from './fake.js';
+
+const sizeFormatter: VxeColumnPropTypes.Formatter<FileItem> = ({ cellValue }) => {
+    // {cellValue, column, row, rowIndex}
+    if (!cellValue) {
+        return ""
+    }
+    return formatFileSize(cellValue)
 }
 
-const timeFormater = (_row: FileItem, _column: TableColumnCtx<FileItem>, cellVaule: number | undefined, _index: number) => {
-    if (!cellVaule) {
+const timeFormater: VxeColumnPropTypes.Formatter<FileItem> = ({ cellValue }) => {
+    if (!cellValue) {
         return "UNKNOWN"
     }
-    return formatDate(cellVaule)
+    return formatDate(cellValue)
 }
 
-const fileTable = ref(null)
-const colHideFolder = ref(false)
 
-watch(colHideFolder, () => {
-    const t = fileTable.value as any;
-    if (t) {
-        nextTick(() => {
-            t.doLayout()
-        })
-    }
-})
+const isHideFolder = ref(false)
+const isOnlyPreview = ref(false)
 
-const tableData = [
-    {
-        file: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc.txt",
-        modifyTime: 1701847341206,
-        createTime: 1701847341206,
-        size: 556600,
-        folder: "/1/2/3",
-        preview: "1111111111111111111111111456.abc",
 
-    },
 
-]
 </script>
 
 <style lang="less" scoped>
 .table {
     min-width: 10%;
+    max-width: 99%;
+
+}
+
+.filters {
+    margin: 4px 0 8px 0;
+
+    .el-checkbox {
+        margin-right: 12px;
+    }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import "./style.css";
 import "element-plus/dist/index.css";
 
+import VXETable from "vxe-table";
+import "vxe-table/lib/style.css";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(VXETable).mount("#app");
