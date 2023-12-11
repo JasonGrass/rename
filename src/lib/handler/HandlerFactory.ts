a56a3749f395df0550b29f37aa4d79a05e0ef8b6
JasonGrass <junjiegrass@qq.com>
2023-12-11 19:43:19 +0800

    调整 handler 默认排序

--- src/lib/handler/HandlerFactory.ts
+++ src/lib/handler/HandlerFactory.ts
@@ -2,6 +2,8 @@ import { reactive } from "vue"
 
 import Handlers from "./HandlerAutoExport"
 
+Handlers.sort((a, b) => a.sortHint - b.sortHint)
+
 export default reactive({
   handlers: Handlers
 })
